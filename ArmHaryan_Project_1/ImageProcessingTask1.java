95e9f218586267bf7ef12d0b16eb119b759ea8d8
Aram Harutyunyan <73275131+Armharyan@users.noreply.github.com>
2023-12-22 13:04:44 +0400

    Add files via upload

--- ArmHaryan_Project_1/ImageProcessingTask1.java
+++ ArmHaryan_Project_1/ImageProcessingTask1.java
@@ -9,7 +9,6 @@ import java.util.List;
 public class ImageProcessingTask1 {
 
     public static void main(String[] args) {
-        // Load the image
         String imagePath = "C:\\Users\\User1\\Desktop\\ImageProcessing-ArmHaryan\\ArmHaryan_Project_1\\H047\\OOP.MT.170317.H047_1.1.jpg";
         Mat originalImage = Imgcodecs.imread(imagePath);
 
@@ -28,100 +27,70 @@ public class ImageProcessingTask1 {
                 ranges,
                 accumulate
         );
-
-        // Remove text
         Mat imageWithoutText = removeText(originalImage);
-
-        // Remove marks
         Mat imageWithoutMarks = removeMarks(imageWithoutText);
-
-        // Extract handwriting
         Mat handwritingImage = extractHandwriting(imageWithoutMarks);
 
-        // Generate output filename
         String dateStr = new SimpleDateFormat("ddMMyy").format(new Date());
         String outputFilename = "CRS.TST." + dateStr + ".C001_p1_bin.png";
 
-        // Crop handwriting and save as binary image
         cropAndSave(handwritingImage, outputFilename);
     }
 
     private static Mat removeText(Mat image) {
-        // Implement a method to remove text (you might use a combination of techniques like thresholding, morphology, etc.)
-        // Example: Using adaptive thresholding
         Mat grayImage = new Mat();
-        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
-
-        Mat thresh = new Mat();
-        Imgproc.threshold(grayImage, thresh, 200, 255, Imgproc.THRESH_BINARY_INV);
-
         Mat result = new Mat();
+        Mat thresh = new Mat();
+
+        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.threshold(grayImage, thresh, 200, 255, Imgproc.THRESH_BINARY_INV);
         Core.bitwise_and(image, image, result, thresh);
 
         return result;
     }
 
     private static Mat removeMarks(Mat image) {
-        // Implement a method to remove marks (you might use color-based segmentation)
-        // Example: Using inRange for red shades
         Scalar lowerRed = new Scalar(0, 0, 100);
         Scalar upperRed = new Scalar(100, 100, 255);
         Mat mask = new Mat();
-        Core.inRange(image, lowerRed, upperRed, mask);
-
         Mat result = new Mat();
+        Core.inRange(image, lowerRed, upperRed, mask);
         Core.bitwise_and(image, image, result, mask);
-
         return result;
     }
 
     private static Mat extractHandwriting(Mat image) {
-        // Implement a method to extract handwriting (you might use edge detection, contour analysis, etc.)
-        // Example: Using Canny edge detection
         Mat edges = new Mat();
-        Imgproc.Canny(image, edges, 30, 100);
-
         Mat result = new Mat();
+        Imgproc.Canny(image, edges, 30, 100);
         Core.bitwise_and(image, image, result, edges);
-
         return result;
     }
 
     private static void cropAndSave(Mat image, String filename) {
-        // Convert to grayscale
         Mat grayImage = new Mat();
-        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
-
-        // Thresholding
         Mat thresh = new Mat();
+        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
         Imgproc.threshold(grayImage, thresh, 127, 255, Imgproc.THRESH_BINARY);
-
-        // Find contours
         List<MatOfPoint> contours =  new ArrayList<>();
         Imgproc.findContours(thresh, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        // Sort contours by area
         contours.sort((contour1, contour2) -> Double.compare(Imgproc.contourArea(contour2), Imgproc.contourArea(contour1)));
 
-        // Choose the top contour (you may adjust the number based on your needs)
         int numContoursToConsider = 1;
         for (int i = 0; i < Math.min(numContoursToConsider, contours.size()); i++) {
             Mat contour = contours.get(i);
             double area = Imgproc.contourArea(contour);
 
-            // Filter out small contours
-            if (area > 100) {  // Adjust the threshold as needed
+            if (area > 100) {
                 Rect boundingRect = Imgproc.boundingRect(contour);
                 Mat croppedImage = new Mat(image, boundingRect);
-
-                // Convert to binary format
                 Mat binaryImage = new Mat();
                 Imgproc.cvtColor(croppedImage, binaryImage, Imgproc.COLOR_BGR2GRAY);
                 Imgproc.threshold(binaryImage, binaryImage, 0, 255, Imgproc.THRESH_BINARY | Imgproc.THRESH_OTSU);
 
-                // Save the cropped binary image
                 Imgcodecs.imwrite(filename, binaryImage);
-                break;  // Only process the top contour
+                break;
             }
         }
     }
--- ArmHaryan_Project_1/ImageProcessingTask2.java
+++ ArmHaryan_Project_1/ImageProcessingTask2.java
@@ -14,26 +14,18 @@ import java.util.List;
 public class ImageProcessingTask2 {
 
     public static void main(String[] args) {
-        // Load the image
         String imagePath = "C:\\Users\\User1\\Desktop\\ImageProcessing-ArmHaryan\\ArmHaryan_Project_1\\H047\\OOP.MT.170317.H047_1.1.jpg";
         Mat image = Imgcodecs.imread(imagePath);
-
-        // Convert the image to grayscale
         Mat grayImage = new Mat();
-        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
-
-        // Apply Gaussian blur for smoothing
-        Imgproc.GaussianBlur(grayImage, grayImage, new Size(5, 5), 0);
-
-        // Apply Canny edge detection for detecting edges
         Mat edges = new Mat();
+
+        Imgproc.cvtColor(image, grayImage, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.GaussianBlur(grayImage, grayImage, new Size(5, 5), 0);
         Imgproc.Canny(grayImage, edges, 50, 150);
 
-        // Find contours in the edges
         List<MatOfPoint> contours = new ArrayList<>();
         Imgproc.findContours(edges, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        // Find the contour with the maximum area (assuming it corresponds to the page)
         double maxArea = -1;
         int maxAreaIdx = -1;
         for (int i = 0; i < contours.size(); i++) {
@@ -44,13 +36,9 @@ public class ImageProcessingTask2 {
             }
         }
 
-        // Get the bounding box of the page
         Rect boundingBox = Imgproc.boundingRect(contours.get(maxAreaIdx));
-
-        // Draw the bounding box on the original image
         Imgproc.rectangle(image, boundingBox.tl(), boundingBox.br(), new Scalar(0, 255, 0), 2);
 
-        // Calculate the orientation of the page
         MatOfPoint2f approxCurve = new MatOfPoint2f();
         MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(maxAreaIdx).toArray());
         double epsilon = 0.02 * Imgproc.arcLength(contour2f, true);
@@ -58,13 +46,9 @@ public class ImageProcessingTask2 {
         Point[] points = approxCurve.toArray();
         double angle = Math.atan2(points[1].y - points[0].y, points[1].x - points[0].x);
 
-        // Print the angle
         System.out.println("Orientation Angle: " + Math.toDegrees(angle));
-
-        // Print the bounding box size
         System.out.println("Bounding Box Size: " + boundingBox.size());
 
-        // Display the processed image
         Imgcodecs.imwrite("C:\\Users\\User1\\Desktop\\ImageProcessing-ArmHaryan\\ArmHaryan_Project_1\\H047\\OOP.MT.170317.H047_1.1.jpg", image);
     }
 }
